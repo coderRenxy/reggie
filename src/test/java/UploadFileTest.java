37df6661cf7536200e225b11b9474ad711cd5c0c
coder.Ren <1074136734@qq.com>
2023-04-13 09:30:55 +0800

    测试上传

--- src/test/java/UploadFileTest.java
+++ src/test/java/UploadFileTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 public class UploadFileTest {
     @Test
     public void test1(){
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
         String fileName = "hhh.jpg.png";
         System.out.println(fileName.substring(fileName.lastIndexOf('.')));
     }
